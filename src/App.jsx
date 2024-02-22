4ffd706a99b26effb6501942d4a073b7f296e9cb
aay7ush <aay7ush@gmail.com>
2024-02-22 16:48:33 +0530

    feat: setup react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ChannelDetail from './pages/ChannelDetail'
+import Feed from './pages/Feed'
+import Layout from './pages/Layout'
+import SearchFeed from './pages/SearchFeed'
+import VideoDetail from './pages/VideoDetail'
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Feed />} />
+          <Route path="video/:id" element={<VideoDetail />} />
+          <Route path="channel/:id" element={<ChannelDetail />} />
+          <Route path="search/:searchTerm" element={<SearchFeed />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 export default App
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router'
+
+const Layout = () => {
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
+export default Layout
