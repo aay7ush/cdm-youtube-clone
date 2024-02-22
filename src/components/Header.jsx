1f2a2c6ad072933a33090103bfb0023eb62b5567
aay7ush <aay7ush@gmail.com>
2024-02-22 17:07:36 +0530

    feat: add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,15 @@
+import { Link } from 'react-router-dom'
+import SearchBar from './SearchBar'
+
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header className="sticky top-0 flex justify-between px-5 py-3">
+      <Link to="/">
+        <img src="/logo.svg" alt="" className="w-32 h-10" />
+      </Link>
+
+      <SearchBar />
+    </header>
+  )
 }
 export default Header
