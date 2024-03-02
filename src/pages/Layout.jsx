7715cd8e3700cc25871f813b496ddf8d9d24d4a9
aay7ush <aay7ush@gmail.com>
2024-03-03 00:36:55 +0530

    add layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router'
-import Header from '../components/Header'
+import { Link } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <>
-      <Header />
+      <header className="sticky top-0 px-5 py-2 bg-white">
+        <Link to="/">
+          <img src="/logo.svg" alt="logo" className="w-32 h-10" />
+        </Link>
+      </header>
       <main>
         <Outlet />
       </main>
