7f8ba45799168e8ec21be2bbbb256c0cc736dc24
aay7ush <aay7ush@gmail.com>
2024-03-03 00:37:52 +0530

    add feed with sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,53 @@
+import {
+  Code,
+  Dumbbell,
+  Flame,
+  Gamepad2,
+  GraduationCap,
+  HomeIcon,
+  Music,
+  Newspaper,
+  Popcorn,
+  ShoppingBag,
+  Trophy,
+} from 'lucide-react'
+
+const categories = [
+  { name: 'New', icon: <HomeIcon /> },
+  { name: 'Trending', icon: <Flame /> },
+  { name: 'Coding', icon: <Code /> },
+  { name: 'Shopping', icon: <ShoppingBag /> },
+  { name: 'Music', icon: <Music /> },
+  { name: 'Movies', icon: <Popcorn /> },
+  { name: 'Gaming', icon: <Gamepad2 /> },
+  { name: 'News', icon: <Newspaper /> },
+  { name: 'Sport', icon: <Trophy /> },
+  { name: 'Education', icon: <GraduationCap /> },
+  { name: 'Gym', icon: <Dumbbell /> },
+]
+
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => {
+  return (
+    <div className="flex md:flex-col md:gap-1 px-5 py-2 overflow-scroll md:overflow-visible scrollbar-hide sticky top-14 h-min bg-white">
+      {categories.map((category) => (
+        <button
+          onClick={() => setSelectedCategory(category.name)}
+          key={category.name}
+          className={`flex gap-2 font-medium px-3 py-2 rounded-full transition duration-300 hover:bg-red-600 hover:text-white group ${
+            category.name === selectedCategory && 'bg-red-600 text-white'
+          }`}
+        >
+          <span
+            className={`group-hover:text-white ${
+              category.name === selectedCategory ? 'text-white' : 'text-red-600'
+            }`}
+          >
+            {category.icon}
+          </span>
+          <span>{category.name}</span>
+        </button>
+      ))}
+    </div>
+  )
+}
+export default Sidebar
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import Videos from '../components/Videos'
+import fetchAPI from '../lib/fetchAPI'
+
 const Feed = () => {
-  return <div>Feed</div>
+  const [selectedCategory, setSelectedCategory] = useState('New')
+  const [videos, setVideos] = useState(null)
+
+  useEffect(() => {
+    fetchAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    )
+  }, [selectedCategory])
+
+  return (
+    <div className="flex flex-col md:flex-row">
+      <Sidebar
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <div>
+        <h4 className="text-4xl font-medium text-center md:text-start mb-3 space-x-2">
+          <span>{selectedCategory === 'Home' ? 'New' : selectedCategory}</span>
+          <span className="text-red-600">videos</span>
+        </h4>
+        <Videos videos={videos} />
+      </div>
+    </div>
+  )
 }
 export default Feed
